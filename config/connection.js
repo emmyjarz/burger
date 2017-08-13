37f2dc17bcbee632896871d427d138502539872d
unknown <emmyjarz@gmail.com>
2017-08-13 12:19:52 -0700

    update connection

--- config/connection.js
+++ config/connection.js
@@ -1,31 +1,21 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-//Use heroku
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
-    if (err) throw err;
-
-    console.log('The solution is: ', rows[0].solution);
+var connection = mysql.createConnection({
+    port: 3306,
+    host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "dg182jof6gj6kabh",
+    password: "ev6vbt0gwjvmx5e7",
+    database: "burgers_db"
 });
 
-//Use local machine to connect
-// var connection = mysql.createConnection({
-//     port: 3306,
-//     host: "localhost",
-//     user: "root",
-//     password: "milomilo",
-//     database: "burgers_db"
-// });
-
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error("error connecting: " + err.stack);
-//         return;
-//     }
-//     console.log("connected as id " + connection.threadId);
-// });
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+});
 
 
 // Export connection for our ORM to use.
