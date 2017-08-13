3548ae4b7fb98c04cd37dd44b1cead1bd4cb7bc9
unknown <emmyjarz@gmail.com>
2017-08-13 12:32:04 -0700

    fix database connection

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "dg182jof6gj6kabh",
-    password: "ev6vbt0gwjvmx5e7",
-    database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "milomilo",
+        database: "burgers_db"
+    });
+} 
 
 connection.connect(function (err) {
     if (err) {
